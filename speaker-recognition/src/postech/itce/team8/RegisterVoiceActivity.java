ee151febc7e727267e271505c07da833e5446b53
hiepnh.vtt@gmail.com <hiepnh.vtt@gmail.com@120febe0-a655-af85-dc0f-037a25d1ef83>
2012-10-15 15:19:19 +0000

    Audio Recorder: OK

--- speaker-recognition/src/postech/itce/team8/RegisterVoiceActivity.java
+++ speaker-recognition/src/postech/itce/team8/RegisterVoiceActivity.java
@@ -1,32 +1,64 @@
 package postech.itce.team8;
 
+import postech.itce.team8.util.AudioRecorder;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class RegisterVoiceActivity extends Activity {
-
-	//
+	private static final String LOG_TAG = "RegisterVoiceActivity";
+	//UI controls
+	private Button btnRecord;
+	private Button btnStop;
+	private Button btnPlay;
+	private Button btnNext;
 	
 	private Button btnFinish;
 	private Button btnCancel;
+	//audio recorder
+	private AudioRecorder audioRecorder;
+	//file uploader
+	
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_voice);
         //
+        btnRecord = (Button)findViewById(R.id.btnRecord);
+        btnStop = (Button)findViewById(R.id.btnStop);
+        btnPlay = (Button)findViewById(R.id.btnPlay);
+        btnNext = (Button)findViewById(R.id.btnNext);
         
         btnFinish = (Button)findViewById(R.id.btnFinish);
         btnCancel = (Button)findViewById(R.id.btnCancel);
         
         Bundle savedBasicInfo = this.getIntent().getExtras();
+        //debug
+        Log.i(LOG_TAG, "fullName=" + savedBasicInfo.get("fullName"));
+        Log.i(LOG_TAG, "userName=" + savedBasicInfo.get("userName"));
+        Log.i(LOG_TAG, "password=" + savedBasicInfo.get("password"));
         
         //
+        
+        
+        //btnRecord
+        btnRecord.setOnClickListener(btnRecordHandler);
+        
+        //btnStop
+        btnStop.setOnClickListener(btnStopHandler);
+        
+        //btnPlay
+        
+        //btnNext
+        
+        
+        //btnFinish
         btnFinish.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -56,7 +88,42 @@ public class RegisterVoiceActivity extends Activity {
         return true;
     }
     
+    //RECORD
+    private void enableButton(int id,boolean isEnable){
+		((Button)findViewById(id)).setEnabled(isEnable);
+	}
+    
+    private void enableButtons(boolean isRecording) {
+		enableButton(R.id.btnRecord,!isRecording);
+		enableButton(R.id.btnStop,isRecording);
+	}
+    
+    
     //UPLOAD
     
     
+    //HANDLERs
+    View.OnClickListener btnRecordHandler = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			Log.i(LOG_TAG, "Start Recording");
+			enableButtons(true);
+			
+			audioRecorder = new AudioRecorder();
+			audioRecorder.startRecording();
+		}
+	};
+	
+	View.OnClickListener btnStopHandler = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			enableButtons(false);
+			
+			audioRecorder.stopRecording();
+			Log.i(LOG_TAG, "Finish recording, filename=" + audioRecorder.getSavedFilename());
+			audioRecorder = null;
+		}
+	};
 }
