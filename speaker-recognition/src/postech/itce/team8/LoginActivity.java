43e956d3b63f7d65fc9efd864094328873a92151
hiepnh.vtt@gmail.com <hiepnh.vtt@gmail.com@120febe0-a655-af85-dc0f-037a25d1ef83>
2012-10-17 09:25:45 +0000

    LoginActivity: in progress

--- speaker-recognition/src/postech/itce/team8/LoginActivity.java
+++ speaker-recognition/src/postech/itce/team8/LoginActivity.java
@@ -1,20 +1,298 @@
 package postech.itce.team8;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import postech.itce.team8.util.AudioRecorder;
+import postech.itce.team8.util.FileUpload;
+import postech.itce.team8.util.HttpPostRequester;
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+	private static final String LOG_TAG = "LoginActivity";
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+	//
+	private static final int ID_DIALOG_LOADING = 0;
+	
+	//private static final String UPLOAD_URL = "http://141.223.83.139:8080/itce600_server/UploadServlet";	
+	//119.202.84.55
+	//172.168.148.228
+	private static final String UPLOAD_URL = "http://119.202.84.125:8080/itce600-team8/doctor/executeUpload.do";
+	private static final String IDENTIFY_URL = "http://119.202.84.125:8080/itce600-team8/doctor/enrollVoice.do";
+	
+	//
+	final Context context = this;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_login, menu);
-        return true;
+	SharedPreferences settings;
+
+	//
+	private Handler mHandler;
+	//audio recorder
+	private AudioRecorder audioRecorder;
+	private String audioRecorderFolder;
+	
+	// UI controls
+	private Spinner spRecentUsers;
+	private TextView lblCaptcha;
+	private Button btnRecord;
+	private Button btnStop;
+
+	//
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		//
+		spRecentUsers = (Spinner) findViewById(R.id.spRecentUsers);
+		lblCaptcha = (TextView) findViewById(R.id.lblCaptcha);
+		btnRecord = (Button) findViewById(R.id.btnRecord);
+		btnStop = (Button) findViewById(R.id.btnStop);
+
+		//
+		settings = PreferenceManager.getDefaultSharedPreferences(context);
+		String recentUsersListStr = settings.getString("recentUsersList",
+				"Other");
+		String[] userNameList = recentUsersListStr.split(";");
+
+		List<String> recentUsersList = new ArrayList<String>();
+		for (String userName : userNameList)
+			recentUsersList.add(userName);
+
+		//
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, recentUsersList);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spRecentUsers.setAdapter(dataAdapter);
+		spRecentUsers.setOnItemSelectedListener(spRecentUsersHandler);
+
+		//
+		btnRecord.setOnClickListener(btnRecordHandler);
+        btnStop.setOnClickListener(btnStopHandler);
+        
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_login, menu);
+		return true;
+	}
+	
+	//
+	private void updateRecentUsers(String newUserName) {
+		settings = PreferenceManager.getDefaultSharedPreferences(context);
+		String recentUsersListStr = settings.getString("recentUsersList",
+				"Other");
+		String[] userNameList = recentUsersListStr.split(";");
+
+		List<String> recentUsersList = new ArrayList<String>();
+		for (String userName : userNameList)
+			recentUsersList.add(userName);
+		// add newUserName
+		recentUsersList.add(0, newUserName);
+				
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, recentUsersList);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spRecentUsers.setAdapter(dataAdapter);		
+
+		String newUsersListStr = "";
+		for (String userName : recentUsersList)
+			newUsersListStr += userName + ";";
+
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putString("recentUsersList", newUsersListStr);
+		// Commit the edits!
+	    editor.commit();
+
+	}
+	
+	
+	private void enableButton(int id,boolean isEnable){
+		((Button)findViewById(id)).setEnabled(isEnable);
+	}
+	
+	private void enableButtons(boolean isRecording) {
+		enableButton(R.id.btnRecord,!isRecording);
+		enableButton(R.id.btnStop,isRecording);
+	}
+
+	// HANDLERs
+	AdapterView.OnItemSelectedListener spRecentUsersHandler = new AdapterView.OnItemSelectedListener() {
+
+		@Override
+		public void onItemSelected(AdapterView<?> parent, View view, int pos,
+				long id) {
+			if (parent.getItemAtPosition(pos).toString().equals("Other")) {
+				// get prompts.xml view
+				LayoutInflater li = LayoutInflater.from(context);
+				View userNamePrompt = li
+						.inflate(R.layout.dialog_username, null);
+
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+						context);
+
+				// set prompts.xml to alertdialog builder
+				alertDialogBuilder.setView(userNamePrompt);
+
+				final EditText txtUserName = (EditText) userNamePrompt
+						.findViewById(R.id.txtUserName);
+
+				// set dialog message
+				alertDialogBuilder
+						.setTitle("Input User Name")
+						.setCancelable(false)
+						.setPositiveButton("OK",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int id) {
+										// get user input and set it to result
+										// edit text
+										lblCaptcha.setText(txtUserName
+												.getText());
+
+										updateRecentUsers(txtUserName.getText()
+												.toString());
+
+									}
+								})
+						.setNegativeButton("Cancel",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int id) {
+										dialog.cancel();
+									}
+								});
+
+				// create alert dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+
+				// show it
+				alertDialog.show();
+
+			}
+
+		}
+
+		@Override
+		public void onNothingSelected(AdapterView<?> arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+	};
+
+	//btnRecord
+    private View.OnClickListener btnRecordHandler = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			Log.i(LOG_TAG, "Start Recording");
+			enableButtons(true);
+			
+			audioRecorder = new AudioRecorder();
+			audioRecorderFolder = audioRecorder.getAudioRecorderFolder();
+			
+			audioRecorder.startRecording();
+		}
+	};
+	
+	//btnStop
+	private View.OnClickListener btnStopHandler = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			enableButtons(false);
+			
+			audioRecorder.stopRecording("temp.wav");
+			
+			Log.i(LOG_TAG, "Finish recording, filename=" + audioRecorder.getSavedFilename());
+			audioRecorder = null;
+			
+			//upload to server for identification
+			Log.i(LOG_TAG, "Prepare to upload audio files...");
+			
+			String userName = spRecentUsers.getSelectedItem().toString();
+			
+			//1. upload files
+			showDialog(ID_DIALOG_LOADING);
+			startUploadTempFile();
+			
+		}
+	};
+	
+	//
+	private final Runnable postResults = new Runnable() {
+		@Override
+		public void run(){
+			Toast toast = Toast.makeText(getApplicationContext(), "Uploaded temp file", 
+					Toast.LENGTH_SHORT);
+			toast.show();
+			
+			//2. request voice enrollment
+			String userName = spRecentUsers.getSelectedItem().toString();
+			
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("userName", userName);
+			map.put("loginId", "temp");
+			
+			HttpPostRequester.postData(IDENTIFY_URL, map);
+		}
+	};
+	
+	private void startUploadTempFile(){
+    	Thread t = new Thread() {
+			@Override
+			public void run() {
+				//upload all files: 0.wav,1.wav,....
+				String userName = spRecentUsers.getSelectedItem().toString();
+					String fileName = "temp.wav";
+					
+					FileUpload.doFileUpload("/sdcard/AudioRecorder/login/"+fileName,fileName, UPLOAD_URL, userName);	//selectedPath, fileName, urlString
+				
+				//
+				dismissDialog(ID_DIALOG_LOADING);
+				mHandler.post(postResults);
+			}
+		};
+		t.start();
     }
+	
+	@Override
+	protected Dialog onCreateDialog(int id, Bundle bundle) {
+		if(id == ID_DIALOG_LOADING){
+			ProgressDialog loadingDialog = new ProgressDialog(this);
+			loadingDialog.setMessage("Uploading login audio file...");
+			loadingDialog.setIndeterminate(true);
+			loadingDialog.setCancelable(true);
+			return loadingDialog;
+			
+		}
+	    	
+		//
+		return super.onCreateDialog(id);
+	}
+	
 }
