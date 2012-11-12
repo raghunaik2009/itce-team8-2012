ebd923ec819d92071e682300412bd8703bf5b86a
hiepnh.vtt@gmail.com <hiepnh.vtt@gmail.com@120febe0-a655-af85-dc0f-037a25d1ef83>
2012-11-12 05:44:18 +0000

    Upload with HTTPS (Tomcat), self-signed Certificate

--- UploadFile/src/pack/coderzheaven/MainActivity.java
+++ UploadFile/src/pack/coderzheaven/MainActivity.java
@@ -5,10 +5,35 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.KeyStore;
 import java.util.HashMap;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.ContentBody;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import stackoverflow.util.AdditionalKeyStoresSSLSocketFactory;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -21,6 +46,8 @@ import android.util.Log;
 
 public class MainActivity extends Activity {
 
+	private final String SECURE_UPLOAD_URL = "https://141.223.83.139:8443/itce600_server/UploadServlet";
+	
 	private static final int SELECT_AUDIO = 2;
 	String selectedPath = "";
 	String fileName = "";
@@ -30,7 +57,9 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		openGalleryAudio();
+		new UploadFileTask().execute();
+		
+		//openGalleryAudio();
 	}
 
 	public void openGalleryAudio() {
@@ -77,9 +106,9 @@ public class MainActivity extends Activity {
 		int maxBufferSize = 1 * 1024 * 1024;
 		String responseFromServer = "";
 		//String urlString = "http://119.202.84.76/upload_audio_test/upload_audio.php";
-		//String urlString = "http://141.223.83.139/upload_file_test/upload_file.php";
+		String urlString = "http://141.223.83.139/upload_file_test/upload_file.php";	//XAMPP (php)
 		//String urlString = "http://141.223.83.139:8080/upload";		//CherryPy (python)
-		String urlString = "http://141.223.83.139:8080/itce600_server/UploadServlet";	//Tomcat (java)
+		//String urlString = "http://141.223.83.139:8080/itce600_server/UploadServlet";	//Tomcat (java)
 		try {
 			// ------------------ CLIENT REQUEST
 			FileInputStream fileInputStream = new FileInputStream(new File(selectedPath));
@@ -145,12 +174,85 @@ public class MainActivity extends Activity {
 		}
 	}
 	
+	
+	//createAdditionalCertsSSLSocketFactory
+	protected org.apache.http.conn.ssl.SSLSocketFactory createAdditionalCertsSSLSocketFactory() {
+	    try {
+	        final KeyStore ks = KeyStore.getInstance("BKS");
+
+	        // the bks file we generated above
+	        final InputStream in = getApplicationContext().getResources().openRawResource( R.raw.hiepnh_tomcat);  
+	        try {
+	            // don't forget to put the password used above in strings.xml/mystore_password
+	            //ks.load(in, context.getString( R.string.mystore_password ).toCharArray());
+	        	ks.load(in, "asdfgh".toCharArray());
+	        } finally {
+	            in.close();
+	        }
+
+	        return new AdditionalKeyStoresSSLSocketFactory(ks);
+
+	    } catch( Exception e ) {
+	        throw new RuntimeException(e);
+	    }
+	}
+	
+	
+	//doSecureFileUpload (12-11-12)
+	private void doSecureFileUpload() {
+		final SchemeRegistry schemeRegistry = new SchemeRegistry();
+		schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 8080));
+		schemeRegistry.register(new Scheme("https", createAdditionalCertsSSLSocketFactory(), 8443));
+
+		// and then however you create your connection manager, I use ThreadSafeClientConnManager
+		final HttpParams params = new BasicHttpParams();
+		HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+        HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+
+        //
+		final ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager(params,schemeRegistry);
+		
+		DefaultHttpClient httpClient = new DefaultHttpClient(cm, params);
+        
+		
+		//
+		HttpPost httpPost = new HttpPost(SECURE_UPLOAD_URL);
+	    File file = new File("/sdcard/FAML_Sr3.wav");
+
+	    MultipartEntity mpEntity = new MultipartEntity();
+	    ContentBody cbFile = new FileBody(file, "audio/wav");
+	    mpEntity.addPart("file", cbFile);
+
+
+	    try{
+		    httpPost.setEntity(mpEntity);
+		    System.out.println("executing request " + httpPost.getRequestLine());
+		    HttpResponse response = httpClient.execute(httpPost);
+		    HttpEntity resEntity = response.getEntity();
+	
+		    System.out.println(response.getStatusLine());
+		    if (resEntity != null) {
+		    	System.out.println(EntityUtils.toString(resEntity));
+		    }
+		    if (resEntity != null) {
+		    	resEntity.consumeContent();
+		    }
+	    }catch(Exception e){
+	    	Log.e("Debug", "error: " + e.getMessage(), e);
+	    }
+
+	    httpClient.getConnectionManager().shutdown();
+	    
+	}
+	
 	//
 	private class UploadFileTask extends AsyncTask{
 
 		@Override
 		protected Object doInBackground(Object... params) {
-			doFileUpload();
+			//doFileUpload();
+			doSecureFileUpload();
+			
 			return null;
 		}
 		
