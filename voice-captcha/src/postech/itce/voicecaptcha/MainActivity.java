741dc3fe16dc50aa0a01ccb22d4435847678a1fe
hiepnh.vtt@gmail.com <hiepnh.vtt@gmail.com@120febe0-a655-af85-dc0f-037a25d1ef83>
2012-12-13 01:47:23 +0000

    MainActivity.java --> use SpeechRecognizer + RecognizerIntent + RecognitionListener(onBufferReceived[])

--- voice-captcha/src/postech/itce/voicecaptcha/MainActivity.java
+++ voice-captcha/src/postech/itce/voicecaptcha/MainActivity.java
@@ -1,15 +1,104 @@
+//http://stackoverflow.com/questions/4559930/speechrecognizer-causes-anr-i-need-help-with-android-speech-api
+
 package postech.itce.voicecaptcha;
+
+
+
+import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	
+	private static final String TAG = "MainActivity";
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //Speech Recognition 
+        //from: http://stackoverflow.com/questions/5913773/speech-to-text-on-android/5915010#5915010
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
+                "postech.itce.voicecaptcha");
+
+        SpeechRecognizer recognizer = SpeechRecognizer
+                .createSpeechRecognizer(this.getApplicationContext());
+        RecognitionListener listener = new RecognitionListener() {
+            @Override
+            public void onResults(Bundle results) {
+                ArrayList<String> voiceResults = results
+                        .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if (voiceResults == null) {
+                    Log.e(TAG, "No voice results");
+                } else {
+                    Log.d(TAG, "Printing matches: ");
+                    for (String match : voiceResults) {
+                        Log.d(TAG, match);
+                    }
+                }
+            }
+
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+                Log.d(TAG, "Ready for speech");
+            }
+
+            @Override
+            public void onError(int error) {
+                Log.d(TAG,
+                        "Error listening for speech: " + error);
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                Log.d(TAG, "Speech starting");
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+            	Log.d(TAG, "onBufferReceived: len = " + buffer.length);
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+            	Log.d(TAG, "Speech ending");
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+                // TODO Auto-generated method stub
+
+            }
+        };
+        recognizer.setRecognitionListener(listener);
+        recognizer.startListening(intent);
+        
+        Log.d(TAG, "recognizer started");
     }
 
     @Override
